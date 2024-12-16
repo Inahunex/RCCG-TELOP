49129e11d43fcc7fd15238246fc2135e9527509e
inahunex <ubanghainah12345@gmail.com>
2024-12-16 12:54:18 +0100

    the beginning of telop web

---
+++ Telop.js
@@ -0,0 +1,7 @@
+const drpbtn = document.querySelector(".drpbtn")
+const drpitem = document.querySelector(".drpitem")
+
+
+drpbtn.addEventListener("click", (theme) => {
+    drpitem.style.display = "block"
+})
